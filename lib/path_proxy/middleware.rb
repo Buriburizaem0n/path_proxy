5196d3b2a93c01843fa70eddb5a1b96e5f186fb8
zaemon.li <zaemon.li@beko.com>
2025-07-22 17:19:30 +0800

    fix plugins

--- lib/path_proxy/middleware.rb
+++ lib/path_proxy/middleware.rb
@@ -3,27 +3,31 @@ require "uri"
 
 module PathProxy
   class Middleware < Rack::Proxy
-    def initialize(app, opts = {})
+    def initialize(app, _opts = {})
       super(app)
       @app = app
-      @source_prefix = (opts[:source_prefix] || "/path").chomp("/")
-      @target_base   = opts[:target_base]   || "http://127.0.0.1:8551"
-      @target_uri    = URI(@target_base)
     end
 
     def call(env)
       req = Rack::Request.new(env)
 
-      unless SiteSetting.path_proxy_enabled && proxy_target?(req.path)
+      # 在这里再访问 SiteSetting，保证它已加载
+      source_prefix = SiteSetting.path_proxy_source_prefix.chomp("/")
+      target_base   = SiteSetting.path_proxy_target_base
+      enabled       = SiteSetting.path_proxy_enabled
+
+      unless enabled && (req.path == source_prefix || req.path.start_with?(source_prefix + "/"))
         return @app.call(env)
       end
 
-      Rails.logger.warn "[path_proxy] proxying #{req.fullpath} -> #{@target_base}"
+      Rails.logger.warn "[path_proxy] proxying #{req.fullpath} -> #{target_base}"
 
-      backend_url = build_backend_url(req)
-      env["HTTP_HOST"]       = @target_uri.host
-      env["SERVER_PORT"]     = @target_uri.port.to_s
-      env["rack.url_scheme"] = @target_uri.scheme
+      target_uri = URI(target_base)
+      backend_url = build_backend_url(req, source_prefix, target_base)
+
+      env["HTTP_HOST"]       = target_uri.host
+      env["SERVER_PORT"]     = target_uri.port.to_s
+      env["rack.url_scheme"] = target_uri.scheme
       env["REQUEST_URI"]     = backend_url.request_uri
       env["PATH_INFO"]       = backend_url.path
       env["QUERY_STRING"]    = backend_url.query.to_s
@@ -34,14 +38,10 @@ module PathProxy
 
     private
 
-    def proxy_target?(path)
-      path == @source_prefix || path.start_with?(@source_prefix + "/")
-    end
-
-    def build_backend_url(req)
-      suffix = req.path.sub(@source_prefix, "")
+    def build_backend_url(req, source_prefix, target_base)
+      suffix = req.path.sub(source_prefix, "")
       suffix = "/" if suffix.empty?
-      URI.join(@target_base, suffix + (req.query_string.empty? ? "" : "?#{req.query_string}"))
+      URI.join(target_base, suffix + (req.query_string.empty? ? "" : "?#{req.query_string}"))
     end
   end
 end
--- plugin.rb
+++ plugin.rb
@@ -1,12 +1,10 @@
 # frozen_string_literal: true
 # name: path_proxy
-# version: 0.1
 # about: Proxy /path/* to an internal service
+# version: 0.1
 # authors: You
 
-gem 'rack-proxy', '0.7.6', require: 'rack/proxy'
-
-enabled_site_setting :path_proxy_enabled
+gem 'rack-proxy', '0.7.6', require: 'rack/proxy'  # 官方推荐在 plugin.rb 声明 gem。:contentReference[oaicite:1]{index=1}
 
 require_relative "lib/path_proxy/middleware"
 
@@ -15,19 +13,17 @@ module ::PathProxy
     engine_name "path_proxy"
     isolate_namespace PathProxy
 
-    # 这里在 Rails 初始化很早的阶段执行，middleware 还没被冻结
+    # 不要在这里用 SiteSetting
     config.before_initialize do |app|
       app.middleware.insert_before(
         0,
-        ::PathProxy::Middleware,
-        source_prefix: SiteSetting.path_proxy_source_prefix.chomp("/"),
-        target_base:   SiteSetting.path_proxy_target_base
+        ::PathProxy::Middleware
       )
     end
   end
 end
 
-# 这里不要再动 middleware，只要让 Discourse 加载文件即可
+# 这里不再动 middleware
 after_initialize do
   # nothing
 end
